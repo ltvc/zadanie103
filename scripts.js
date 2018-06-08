a9b04e9e0fe91129e4b6c724b888f3aa32ded929
ltvc <38085145+ltvc@users.noreply.github.com>
2018-06-08 17:30:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 $(function() {
   var carouselList = $('#carousel ul');
-});
 function moveFirstSlide() {
       var firstItem = carouselList.find('li:first');
       var lastItem = carouselList.find('li:last');
       lastItem.after(firstItem);
       carouselList.css({marginLeft:0});
-    });
+    };
 function changeSlide () {
-	moveFirstSlide();
-   carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-  });
+	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+  };
   setInterval(changeSlide, 1000);
+});
